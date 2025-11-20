5a0162bc8144ec27d11b2c08c7fc0e7430e17c1a
BlineraMoberg <bm222mr@student.lnu.se>
2025-11-20 13:47:12 +0100

    bug found in wrong length

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,8 +1,9 @@
- import { SSNHelper } from '../src/correct/SSNHelper'; 
+// import { SSNHelper } from '../src/correct/SSNHelper'; 
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30'
 // import { SSNHelper  } from '../src/bugs/BuggySSNHelperAllowMonth0'
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat'
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperMessyLuhn'
+ import { SSNHelper } from '../src/bugs/BuggySSNHelperWrongLength'
 
 describe('SSNHelpe Tests', () => {
     const helper = new SSNHelper()
@@ -15,15 +16,23 @@ describe('SSNHelpe Tests', () => {
         expect(helper.isValidMonth('0')).toBeFalsy()
     })
 
-    test(' isCorrectFormat Should Return False For Missing Dash', () => {
+    test('isCorrectFormat Should Return False For Missing Dash', () => {
         const actual = helper.isCorrectFormat('9009212334')
         expect(actual).toBeFalsy()
     })
 
-    test(' luhnisCorrect should Return False For incorrect luhn number', () => {
+    test('luhnisCorrect Should Return False For Incorrect Luhn Number', () => {
         const actual = helper.luhnisCorrect('950726-2500')
-        expect (actual).toBeTruthy()
+        expect(actual).toBeTruthy()
     })
 
-    test('')
+    test('isCorrectLength Length Should Return False For Too Long Input', () => {
+        const helper = new SSNHelper()
+        const actual = helper.isCorrectLength('19970930-12345')
+        expect(actual).toBeFalsy()
+    })
+
+    // test('isCorrectLength Length Should Return False For Too Short Input', () => {
+
+    // })
 });
