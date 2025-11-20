b766492b0cf577f0f913f9af8c4c6761144b01c7
BlineraMoberg <bm222mr@student.lnu.se>
2025-11-20 11:56:42 +0100

    found the bug in luhn

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,13 +1,14 @@
-// import { SSNHelper } from '../src/correct/SSNHelper'; 
+ import { SSNHelper } from '../src/correct/SSNHelper'; 
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30'
 // import { SSNHelper  } from '../src/bugs/BuggySSNHelperAllowMonth0'
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat'
+// import { SSNHelper } from '../src/bugs/BuggySSNHelperMessyLuhn'
 
 describe('SSNHelpe Tests', () => {
     const helper = new SSNHelper()
 
     test('isValidDay Should Return True For Boundary 31', () => {
-        expect(helper.isValidDay('31')).toBe(true)
+        expect(helper.isValidDay('31')).toBeTruthy()
     })
 
     test('isValidMonth Should Return False For Month Zero', () => {
@@ -18,4 +19,11 @@ describe('SSNHelpe Tests', () => {
         const actual = helper.isCorrectFormat('9009212334')
         expect(actual).toBeFalsy()
     })
+
+    test(' luhnisCorrect should Return False For incorrect luhn number', () => {
+        const actual = helper.luhnisCorrect('950726-2500')
+        expect (actual).toBeTruthy()
+    })
+
+    test('')
 });
