71d28a817824da68cae85221e9c057a7cfcf4ac7
BlineraMoberg <bm222mr@student.lnu.se>
2025-11-20 10:33:44 +0100

    in the second test i found the bug in isValidDay and isValidMonth

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,12 +1,17 @@
-import { SSNHelper } from '../src/correct/SSNHelper'; 
-
-
+// import { SSNHelper } from '../src/correct/SSNHelper'; 
+// import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30'
+// import { SSNHelper  } from '../src/bugs/BuggySSNHelperAllowMonth0'
+import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat'
 
 describe('SSNHelpe Tests', () => {
 
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
-    });
+    test('isValidDay returns true for upper boundary 31', () => {
+        const helper = new SSNHelper()
+        expect(helper.isValidDay('31')).toBe(true)
+    })
 
-    //Add your tests here
+    test('isValidMonth return false for lower boundary 0', () => {
+        const helper = new SSNHelper()
+        expect(helper.isValidMonth('0')).toBeFalsy()
+    })
 });
