76afd85f682c9da2527ea803269305f67736ac89
BlineraMoberg <bm222mr@student.lnu.se>
2025-11-24 14:37:07 +0100

    the mock done with getYear

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,7 +1,9 @@
 import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'
-import { jest } from '@jest/globals'
+import { expect, jest } from '@jest/globals'
 //import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'
 //import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'
 
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
@@ -26,5 +28,18 @@ describe('SwedishSocialSecurityNumber Tests', () => {
         new SwedishSocialSecurityNumber(' 990824-3684 ', helper)
         expect(helper.isCorrectLength).toHaveBeenCalledWith('990824-3684')
      })
-});
 
+     test('Constructor Should luhn the SocialSecurityNumber', () => {
+        helper.luhnisCorrect.mockReturnValue(false)
+        expect(()=> 
+        new SwedishSocialSecurityNumber('950726-2500', helper)
+    ).toThrow()
+     })
+
+     test('getYear Should Return Correct Year', () => {
+        helper.luhnisCorrect.mockReturnValue(true)
+        const validYear = new SwedishSocialSecurityNumber('950726-2500', helper).getYear()
+        expect(validYear).toBe('95')
+
+     })
+});
