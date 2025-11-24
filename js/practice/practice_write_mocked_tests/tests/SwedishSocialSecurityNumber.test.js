8ed46b3145299824ddb0d9006641372e7090a821
BlineraMoberg <bm222mr@student.lnu.se>
2025-11-24 14:01:39 +0100

    the mock done with trim

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,6 +1,7 @@
-//import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'
+import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'
 import { jest } from '@jest/globals'
-import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck';
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'
+//import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
 
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
@@ -11,6 +12,7 @@ describe('SwedishSocialSecurityNumber Tests', () => {
         isCorrectFormat: jest.fn(),
         luhnisCorrect: jest.fn()
     }
+    const newSecurityNr = ('990824-3684')
 
     test('Constructor Should Throw For Incorrect Length', () => {
         helper.isCorrectLength.mockReturnValue(false)
@@ -18,5 +20,11 @@ describe('SwedishSocialSecurityNumber Tests', () => {
         new SwedishSocialSecurityNumber('9701-1234', helper)
     ).toThrow()
     })
+
+    test('constructor Should TrimSpaces ForInputWithLeadingAndTrailingSpaces', () => {
+        helper.isCorrectLength.mockReturnValue(true)
+        new SwedishSocialSecurityNumber(' 990824-3684 ', helper)
+        expect(helper.isCorrectLength).toHaveBeenCalledWith('990824-3684')
+     })
 });
 
